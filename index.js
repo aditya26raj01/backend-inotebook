2448e93f0db989b87db39eb357fb1cdb5e670947
Aditya <you@example.com>
2022-10-24 01:57:26 +0530

    get and post requests

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ connnectToMongo();
 const express = require("express")
 const app = express()
 const port = 3000
-
-
+app.use(express.json());
 
 // Available Routes
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/",(req,res)=>{
-    res.json([]);
+
+// Create a User using: POST "/api/auth" Doesnt Require Auth
+router.post("/",(req,res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send("Hello")
 })
 
 module.exports = router;
