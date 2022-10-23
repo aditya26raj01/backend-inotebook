36e1b6f03730e9091634e6349141882c6148d0a6
Aditya <you@example.com>
2022-10-24 03:17:31 +0530

    authentication till jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,52 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/User");
+const express = require("express")
+const router = express.Router()
+const User = require("../models/User")
+const { body, validationResult } = require("express-validator")
+const bcrypt = require("bcryptjs")
+var JWT = require("jsonwebtoken")
+
+const JWT_SECRET = "harryisagoodb$oy"
+
+// Create a User using: POST "/api/auth/createuser" Doesnt Require Auth
 
 
-// Create a User using: POST "/api/auth" Doesnt Require Auth
-router.post("/",(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send("Hello")
+router.post("/createuser", [
+    body("name", "Enter a Valid Name").isLength({ min: 3 }),
+    body("email", "Enter a Valid Email").isEmail(),
+    body("password", "Password must be of 5 characters").isLength({ min: 5 })
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    try {
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists!" })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const securedPassword = await bcrypt.hash(req.body.password, salt)
+
+        // Create a New User
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: securedPassword,
+        })
+
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+
+        const authToken = JWT.sign(data,JWT_SECRET)
+
+        res.json({authToken:authToken})
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some Error Occured!")
+    }
 })
 
-module.exports = router;
+module.exports = router
